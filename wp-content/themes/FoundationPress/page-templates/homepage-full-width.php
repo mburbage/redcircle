b65035b46b66c48b058f07c06686095424ab76a8
mburbage <michael@redcircle.biz>
2019-08-25 15:06:35 -0400

    Git Submod

---
+++ wp-content/themes/FoundationPress/page-templates/homepage-full-width.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Homepage Full Width
+*/
+get_header(); ?>
+
+<?php get_template_part( 'template-parts/featured-image' ); ?>
+<div class="main-container">
+	<div class="main-grid">
+		<main>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/content', 'homepage' ); ?>
+				<?php comments_template(); ?>
+			<?php endwhile; ?>
+		</main>
+	</div>
+</div>
+<?php get_footer();
