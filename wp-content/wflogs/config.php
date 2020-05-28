224155583a60de40c40f845e315f57016cb7cea4
mburbage <michael@redcircle.biz>
2020-05-28 08:02:46 -0400

    Update gitignore

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"c0j]nV`rHB/Io+~oq*P^Lf&lqI.yk~Nb$:m,WQNErSz7c0}l1@]Z^zP3e+HPu?0J";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1080;}
